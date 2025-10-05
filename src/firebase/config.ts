905a402d62783556e14bb285323d102acd5e53f7
CHAKRADHAR GOUD KAKKULURU <kakkuluruchakradhargoud@gmail.com>
2025-10-05 12:59:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5950245941-ba89a",
+  "appId": "1:151104162675:web:fedd9e55332524fc341306",
+  "apiKey": "AIzaSyDedVzdYc_q2reQQf3hwjtdTJR_icL4-Ng",
+  "authDomain": "studio-5950245941-ba89a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "151104162675"
+};
